ce8ff91b190e9078b68d82569409f1005f8d222f
Shin <shin@ShinnoMacBook-Air.local>
2023-05-31 09:41:53 +0900

    0523提出後修正

--- js/script.js
+++ js/script.js
@@ -5,10 +5,13 @@ console.log("test1")
 const btn = document.querySelector('.p-button-menu');
 const menu = document.querySelector('.l-nav');
 const overlay = document.querySelector('.p-menu__bg');
+const open = document.querySelector('body')
+
 btn.addEventListener('click', () => {
     console.log("test2")
 overlay.classList.toggle('overlay');
 menu.classList.toggle('open-menu');
+open.classList.toggle('open-menu');
 
 }); 
 
@@ -19,4 +22,6 @@ const close = document.querySelector('.c-btn_menu-close');
 close.addEventListener('click',() => {
     menu.classList.remove('open-menu');
     overlay.classList.remove('overlay');
+    open.classList.toggle('open-menu');
+
 });
